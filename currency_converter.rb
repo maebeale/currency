150c2e49ce21fe0dc3a9ab6aa62f64fa3f9ef419
maebeale <maebeale@gmail.com>
2015-01-22 10:35:50 -0500

    currency convert method works

--- currency_converter.rb
+++ currency_converter.rb
@@ -1,9 +1,28 @@
+module Rates
+  RATES_HASH = { :USD => 2.0, :EUR => 0.74 }
+end
+
+
 class CurrencyConverter
 
-  
-  def initialize(rates)
+  include Rates
+
+  attr_accessor :rates
+    def initialize(rates)
     @rates = rates
   end
 
+  def convert(currency, new_code)
+    current_amount = currency.amount
+    current_code = currency.code
+
+    RATES_HASH.each do |key, value|
+      if key == current_code
+        currency = Currency.new(current_amount * value, new_code)
+      end
+      return currency
+    end
+
+  end
 
 end
--- currency_converter_test.rb
+++ currency_converter_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './currency'
 require './currency_converter'
 #
 class CurrencyConverterTest < ::MiniTest::Test
@@ -9,14 +10,14 @@ class CurrencyConverterTest < ::MiniTest::Test
   end
 
   def test_currency_converter_initializes_with_a_hash
-    rates_hash = { 'USD' => 1.0, 'EUR' => 0.74 }
+    rates_hash = { :USD => 1.0, :EUR => 0.74 }
     converter = CurrencyConverter.new(rates_hash)
   end
 
   def test_currency_convert_method_returns_new_object_with_same_amount_and_code
-    rates_hash = { 'USD' => 1.0, 'EUR' => 0.74 }
+    rates_hash = { :USD => 2.0, :EUR => 0.74 }
     converter = CurrencyConverter.new(rates_hash)
-    assert_equal converter.convert(Currency.new(1, :USD), :USD), Currency.new(1, :USD)
+    assert_equal converter.convert(Currency.new(1, :USD), :USD), Currency.new(2, :USD)
   end
 
 
