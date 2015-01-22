afceb81a87f7e9cfc3b480be30b4606d669b8f3d
maebeale <maebeale@gmail.com>
2015-01-22 18:55:59 -0500

    Currency.new can take a string and parse it, and can still take in amount and code.

--- currency.rb
+++ currency.rb
@@ -1,14 +1,13 @@
 require './errors'
 
 class Currency
-  # include SymbolsHash
-
+  # include SymbolsHash?
   attr_reader :amount, :code
 
   def initialize(*details)
     symbols_hash = {'$' => :USD, '€' => :EUR, '¥' => :JPY } #TODO - make this a module? Rework so USD & CAD can both be in hash
     if details[0].class == String
-      m = details[0].match /(\W*)\s*(\d+\.\d{2})/
+      m = details[0].match /(\W)\s*(\d+\.{0,1}\d{0,2})/
         @amount = m[2].to_i
         @code = symbols_hash[m[1]]
     else #details[0].class == FixNum
