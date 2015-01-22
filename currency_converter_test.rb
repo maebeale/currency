4a04246534c776636317456ed2c4e2affc9c051c
maebeale <maebeale@gmail.com>
2015-01-21 20:32:51 -0500

    added currency_converter test

---
+++ currency_converter_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './currency_converter'
+#
+class CurrencyConverterTest < ::MiniTest::Test
+
+  def test_currency_class_exists
+    assert CurrencyConverter
+  end
+
+  def test_currency_converter_ini
+
+  end
+end
